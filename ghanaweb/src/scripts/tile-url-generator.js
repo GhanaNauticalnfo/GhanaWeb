7da2b9aa4ec45352c1d761dd4b6fbbf228ee4bbf
steen-elv <steen@lundogbendsen.dk>
2018-03-13 16:19:33 +0100

    MARWEB-580 added osm tile caching to service worker

---
+++ ghanaweb/src/scripts/tile-url-generator.js
@@ -0,0 +1,173 @@
+global.window = {};
+global.navigator = {
+    userAgent: ''
+};
+require('ol');
+var extent = require('ol/extent');
+var tilegrid = require('ol/tilegrid');
+var proj = require('ol/proj');
+var tileurlfunction = require('ol/tileurlfunction');
+
+var anExtent = extent.boundingExtent(
+    [
+        proj.fromLonLat([-4.0, 11.0]),
+        proj.fromLonLat([2.2, 11.0]),
+        proj.fromLonLat([2.2, 1.5]),
+        proj.fromLonLat([-4.0, 1.5])
+    ]);
+
+var url = 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var urls = tileurlfunction.expandUrl(url);
+var maxZoom = 17;
+var grid = tilegrid.createXYZ({
+    extent: tilegrid.extentFromProjection('EPSG:3857'),
+    maxZoom: maxZoom,
+    minZoom: undefined,
+    tileSize: undefined
+});
+const tileUrlFunc = tileurlfunction.createFromTemplates(urls, grid);
+
+
+for (let i = 0; i <= maxZoom; i++) {
+// for (let i = 0; i <= 2; i++) {
+    let minX = 99999999;
+    let maxX = 0;
+    let minY = 99999999;
+    let maxY = 0;
+    grid.forEachTileCoord(anExtent, i, function (tileCoord) {
+        // console.log(tileCoord);
+        // console.log(tileUrlFunc(tileCoord));
+        minX = Math.min(tileCoord[1], minX);
+        minY = Math.min(-tileCoord[2] - 1, minY);
+        maxX = Math.max(tileCoord[1], maxX);
+        maxY = Math.max(-tileCoord[2] - 1, maxY);
+    });
+      // console.log([i, minX, maxX, minY, maxY]);
+      console.log('workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\\/'+i+'\\/('+toRegExp(minX, maxX)+')\\/('+toRegExp(minY, maxY)+')\\.png/, osmCacheFirst, \'GET\');');
+
+}
+
+/*
+const reg = /.*[a-c].tile.openstreetmap.org.*\/17\/(64[1-9][0-9][0-9]|640[8-9][0-9]|640[7-9]9|66[0-2][0-9][0-9]|663[0-2][0-9]|6633[0-6])\/(61[6-9][0-9][0-9]|615[1-9][0-9]|615[0-7][6-9]|64[0-8][0-9][0-9]|649[0-7][0-9]|6498[0-9])\.png/;
+const res = reg.test("http://localhost:8080/rest/nw-nm/messages?lang=en&instanceId=urn%3Amrn%3Amcl%3Aservice%3Ainstance%3Adma%3Anw-nm-ghana&mainType=NW&wkt=POLYGON((-0.5889955173492432%206.754420322537541%2C-0.5889955173492432%206.764179628206861%2C-0.5482044826507568%206.764179628206861%2C-0.5482044826507568%206.754420322537541%2C-0.5889955173492432%206.754420322537541))");
+console.log(res);
+*/
+/*
+console.log("input 0-0:       "+ toRegExp(0, 0));
+console.log("input 1-1:       "+ toRegExp(1, 1));
+console.log("input 11-25:     "+ toRegExp(11, 25));
+console.log("input 111-255:   "+ toRegExp(111, 255));
+console.log("input 111-355:   "+ toRegExp(111, 355));
+console.log("input 11-15:     "+ toRegExp(11, 15));
+console.log("input 111-115:   "+ toRegExp(111, 115));
+console.log("input 1110-1150: "+ toRegExp(1110, 1150));
+console.log("input 110-150:   "+ toRegExp(110, 150));
+console.log("input 1101-1501: "+ toRegExp(1101, 1501));
+*/
+/*
+console.log("input 801-1501: "+ toRegExp(801, 1501));
+console.log("input 888-1511: "+ toRegExp(888, 1511));
+console.log("input 1000-2000: "+ toRegExp(1000, 2000));
+*/
+
+function toRegExp(min, max) {
+    const result = [];
+    const minString = min.toString();
+    const maxString = max.toString();
+
+    const digitCountMin = minString.length;
+    const digitCountMax = maxString.length;
+
+    if (digitCountMin === digitCountMax) {
+        handlePosition(0);
+    } else {
+        minToMaxExclusive(minString, []);
+        result.push("|");
+        handleMax(maxString, []);
+    }
+
+    return result.join('');
+
+    function handlePosition(i, digitsHandled, maxDigitsHandled) {
+        digitsHandled = digitsHandled || [];
+        maxDigitsHandled = maxDigitsHandled || [];
+        const digitMin = minString.charAt(i);
+        const digitMax = maxString.charAt(i);
+
+        if (i === digitCountMin) {
+            result.push(digitsHandled);
+        } else if (digitMin === digitMax) {
+            digitsHandled.push(digitMin);
+            maxDigitsHandled.push(digitMax);
+            handlePosition(i+1, digitsHandled, maxDigitsHandled);
+        } else {
+            if (i === digitCountMin - 1) {
+                result.push(digitsHandled.join(""));
+                result.push(getRangeExpression(digitMin, digitMax));
+            } else {
+                minToMaxExclusive(minString.substring(i), digitsHandled);
+                result.push("|");
+                handleMax(maxString.substring(i), maxDigitsHandled);
+            }
+        }
+
+    }
+    function minToMaxExclusive(minNumberString, digitsHandled) {
+        result.push(digitsHandled.join(""));
+        const length = minNumberString.length;
+        const firstDigit = minNumberString.charAt(0);
+        const correspondingMaxDigit = maxString.charAt(digitsHandled.length);
+        const correspondingMaxDigitNumber = Number(correspondingMaxDigit);
+        const firstDigitNumber = Number(firstDigit);
+
+        if (length > 2) {
+            result.push(firstDigit);
+            result.push(getRangeExpression((Number(minNumberString.charAt(1)) + 1), "9"));
+            for (let i = 2; i < length; i++) {
+                result.push("[0-9]");
+            }
+            result.push("|");
+            digitsHandled.push(firstDigit);
+            minToMaxExclusive(minNumberString.substring(1), digitsHandled);
+        } else {
+            const nextDigit = minNumberString.charAt(1);
+            if (correspondingMaxDigitNumber - firstDigitNumber === 1) {
+                result.push(firstDigit);
+            } else if (correspondingMaxDigitNumber > firstDigitNumber) {
+                result.push(getRangeExpression(firstDigit, (correspondingMaxDigitNumber - 1)));
+            } else {
+                result.push(getRangeExpression(firstDigit, "9"));
+            }
+            result.push(getRangeExpression(nextDigit, "9"));
+        }
+    }
+
+    function handleMax(maxNumberString, digitsHandled) {
+        let firstDigit = maxNumberString.charAt(0);
+        digitsHandled = digitsHandled || [];
+        digitsHandled.push(firstDigit);
+        let length = maxNumberString.length;
+
+        if (length > 2) {
+            let number = (Number(maxNumberString.charAt(1)) - 1);
+            if (number >= 0) {
+                result.push(digitsHandled.join(""));
+                result.push(getRangeExpression("0", number));
+                for (let i = 2; i < length; i++) {
+                    result.push("[0-9]");
+                }
+                result.push("|");
+            }
+            handleMax(maxNumberString.substring(1), digitsHandled);
+
+        } else {
+            result.push(digitsHandled.join(""));
+            result.push(getRangeExpression("0", maxNumberString.charAt(1)));
+        }
+
+    }
+
+    function getRangeExpression(from, to) {
+        return Number(from) === Number(to) ? from : "[" + from + "-" + to +"]";
+    }
+}
--- ghanaweb/sw.js
+++ ghanaweb/sw.js
@@ -8,3 +8,30 @@ workboxSW.precache([]);
 
 workboxSW.router.registerRoute(/.*80x15.png/, workboxSW.strategies.staleWhileRevalidate({}), 'GET');
 workboxSW.router.registerRoute(/keycloak.json/, workboxSW.strategies.staleWhileRevalidate({}), 'GET');
+
+const osmCacheFirst = workboxSW.strategies.cacheFirst(
+    {
+        cacheName: 'osm',
+        cacheExpiration: {
+            maxEntries: 3000,
+            maxAgeSeconds: 180 * 24 * 60 * 60
+        }
+});
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/0\/(0)\/(0)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/1\/([0-1])\/(0)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/2\/([1-2])\/(1)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/3\/([3-4])\/(3)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/4\/([7-8])\/(7)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/5\/(1[5-6])\/(1,5)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/6\/(3[1-2])\/(3[0-1])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/7\/(6[2-4])\/(6[0-3])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/8\/(12[5-9])\/(12[0-6])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/9\/(25[0-9])\/(24[0-9]|25[0-3])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/10\/(50[0-9]|51[0-8])\/(49[0-9]|4[8-9][0-9]|50[0-7])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/11\/(10[0-2][1-9]|103[0-6])\/(9[7-9][0-9]|9[6-9][1-9]|100[0-9]|101[0-5])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/12\/(20[0-6][2-9]|207[0-3])\/(1[10-9][0-9][0-9]|19[3-9][0-9]|192[2-9]|20[0-2][0-9]|2030)\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/13\/(40[1-9][0-9]|40[0-3][4-9]|41[0-3][0-9]|414[0-6])\/(39[0-9][0-9]|38[5-9][0-9]|38[4-5][4-9]|40[0-5][0-9]|406[0-1])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/14\/(80[1-9][0-9]|80[0-8]9|82[0-8][0-9]|829[0-2])\/(7[7-9][0-9][0-9]|769[0-9]|76[8-9][8-9]|80[0-9][0-9]|81[0-1][0-9]|812[0-3])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/15\/(160[2-9][0-9]|160[1-7]9|165[0-7][0-9]|1658[0-4])\/(15[4-9][0-9][0-9]|153[8-9][0-9]|153[7-9][6-9]|16[0-1][0-9][0-9]|162[0-3][0-9]|1624[0-7])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/16\/(32[1-9][0-9][0-9]|320[4-9][0-9]|320[3-5]9|330[0-9][0-9]|331[0-5][0-9]|3316[0-8])\/(30[8-9][0-9][0-9]|307[6-9][0-9]|307[5-8][3-9]|32[0-3][0-9][0-9]|324[0-8][0-9]|3249[0-4])\.png/, osmCacheFirst, 'GET');
+workboxSW.router.registerRoute(/.*[a-c].tile.openstreetmap.org.*\/17\/(64[1-9][0-9][0-9]|640[8-9][0-9]|640[7-9]9|66[0-2][0-9][0-9]|663[0-2][0-9]|6633[0-6])\/(61[6-9][0-9][0-9]|615[1-9][0-9]|615[0-7][6-9]|64[0-8][0-9][0-9]|649[0-7][0-9]|6498[0-9])\.png/, osmCacheFirst, 'GET');
